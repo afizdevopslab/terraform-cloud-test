6b297977c533ea8deb3712250d588197a766a776
afizdevopslab <89161578+afizdevopslab@users.noreply.github.com>
2026-03-19 20:58:46 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.1.1"
+    }
+    }
+    }
+
+locals {
+  twotowers = ["sam", "aragorn", "merri"]
+
+}
+
+resource "null_resource" "twotowers" {
+  for_each = toset(local.twotowers)
+  triggers = {
+    name = each.value
+  }
+}
+
+output "twotowers" {
+  value = null_resource.twotowers
+}
